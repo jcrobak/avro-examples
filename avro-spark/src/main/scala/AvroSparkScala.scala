9bfde9b71443d2df2305ff85c7c6513e3d8e0f1b
Joe Crobak <joecrow@gmail.com>
2014-04-13 21:37:45 +0000

    WIP specific record impl

--- avro-spark/src/main/scala/AvroSparkScala.scala
+++ avro-spark/src/main/scala/AvroSparkScala.scala
@@ -7,17 +7,17 @@ import org.apache.avro.mapreduce.AvroKeyInputFormat
 import org.apache.hadoop.io.NullWritable
 import org.apache.commons.lang.StringEscapeUtils.escapeCsv
 
-object AvroSparkScala {
+object WordCountJobAvroGenericSpark {
   def main(args: Array[String]) {
-    val sc = new SparkContext("local", "Avro Spark Scala", System.getenv().get("SPARK_HOME"),
-      List("target/scala-2.10/avro-spark_2.10-1.0.jar"))
+    val sc = new SparkContext("local", "Avro Generic Spark Scala",
+      System.getenv().get("SPARK_HOME"), List("target/scala-2.10/avro-spark_2.10-1.0.jar"))
 
-    val avroRdd = sc.newAPIHadoopFile("file:///Users/joe/Code/avro-cli-examples/twitter.avro",
+    val avroRdd = sc.newAPIHadoopFile("file://%s".format(args(0),
     								  classOf[AvroKeyInputFormat[GenericRecord]],
     								  classOf[AvroKey[GenericRecord]],
     								  classOf[NullWritable])
 
-	val genericRecords = avroRdd.map{case (ak, _) => ak.datum()}
+  	val genericRecords = avroRdd.map{case (ak, _) => ak.datum()}
 
     val wordCounts = genericRecords.map(gr => gr.get("tweet").asInstanceOf[String])
     	.flatMap{tweet: String => tweet.split(" ")}
@@ -27,6 +27,30 @@ object AvroSparkScala {
     val wordCountsFormatted = wordCounts.map{case (word, count) => (escapeCsv(word), count)}
     	.map{case (word, count) => s"$word,$count"}
 
-	wordCountsFormatted.saveAsTextFile("file:///tmp/twitter-wordcount-scala-spark.tsv")    
+  	wordCountsFormatted.saveAsTextFile("file:///tmp/twitter-wordcount-scala-spark.tsv")    
+  }
+}
+
+object WordCountJobAvroSpecificSpark {
+  def main(args: Array[String]) {
+    val sc = new SparkContext("local", "Avro Specific Spark Scala",
+      System.getenv().get("SPARK_HOME"), List("target/scala-2.10/avro-spark_2.10-1.0.jar"))
+
+    val avroRdd = sc.newAPIHadoopFile("file:///Users/joe/Code/avro-cli-examples/twitter.avro",
+                      classOf[AvroKeyInputFormat[GenericRecord]],
+                      classOf[AvroKey[GenericRecord]],
+                      classOf[NullWritable])
+
+    val genericRecords = avroRdd.map{case (ak, _) => ak.datum()}
+
+    val wordCounts = genericRecords.map(gr => gr.get("tweet").asInstanceOf[String])
+      .flatMap{tweet: String => tweet.split(" ")}
+      .map(word => (word, 1))
+      .reduceByKey((a, b) => a + b)
+
+    val wordCountsFormatted = wordCounts.map{case (word, count) => (escapeCsv(word), count)}
+      .map{case (word, count) => s"$word,$count"}
+
+    wordCountsFormatted.saveAsTextFile("file:///tmp/twitter-wordcount-scala-spark.tsv")    
   }
 }
